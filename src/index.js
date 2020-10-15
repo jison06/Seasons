b22aaaab36094706828e8895d9c5f8337094da5b
jison06 <ison.84@buckeyemail.osu.edu>
2020-10-15 14:26:23 -0400

    Convert to class component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    function (position) {
-      console.log(position);
-    },
-    error => console.log(error)
-  );
-  return <div>Latitude: </div>;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      error => console.log(error)
+    );
+  }
+
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
